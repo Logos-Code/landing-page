7672ca540fe3488998c80b7bee5905f85fa3c1eb
leonel_castaneda <leonel_castaneda@condenast.com>
2022-07-22 14:35:40 -0500

    change external libraries for locals

---
+++ js/master.js
@@ -0,0 +1,68 @@
+function scrollToSection(element) {
+    $("html, body").animate({
+        scrollTop: $(element).offset().top
+    }, 1000);
+};
+
+function showServices(serviceId, serviceTitleId) {
+    if (serviceId === "all") {
+        $(".service-item, .servicesThumbnails").show(400);
+    } else {
+        $(".service-item, .servicesThumbnails").hide(400);
+        $("#" + serviceId + ", ." + serviceId).show(400);
+    }
+
+    $(".list-service-titles").removeClass("active");
+    $("#" + serviceTitleId).addClass("active");
+}
+
+function elementIsInViewport(element) {
+    return ($(window).scrollTop() - $(element).offset().top - $(element).height() < 0);
+}
+
+$(window).on("scroll", function () {
+    if (elementIsInViewport("#homeLogo")) {
+        $("#navbarLogo").hide(1000);
+    } else {
+        $("#navbarLogo").show(1000);
+    }
+});
+
+if (!elementIsInViewport("#homeLogo")) {
+    $("#navbarLogo").show();
+}
+
+$(".nav-link").on("click", function () {
+    var sectionTo = $(this).attr("href");
+    scrollToSection(sectionTo);
+});
+
+$("#contactForm").on("submit", (evt) => {
+    evt.preventDefault();
+
+    $.ajax({
+        method: "POST",
+        headers: { 'x-api-key': "a2U8wMJcKgaetboBcutphakT3nfisN7Q8uXL2nQS" },
+        url: "https://landing-contact-email.logoscode.com.mx/send",
+        data: {
+            name: $("#contactName").val(),
+            email: $("#contactEmail").val(),
+            cellphone: $("#contactCellphone").val(),
+            message: $("#contactMessage").val()
+        }
+    }).done((data, textStatus, jqXHR) => {
+        console.log(data, textStatus, jqXHR);
+    }).fail((jqXHR, textStatus, errorThrown) => {
+        console.log(jqXHR, textStatus, errorThrown);
+    }).always(() => {
+        document.getElementById("contactForm").reset();
+
+        $("#contactForm").notify("Gracias por tu mensaje", { position: "top center", className: "success" });
+    });
+});
+
+$(() => {
+    $("#homeVideo").on("playing", () => {
+        $("#overlay").hide(1000, () => { $("#overlay").remove(); });
+    });
+});
